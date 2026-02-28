cc5aaf57e625ae04f06efbe96c5ecfd425b5bb33
jazzpalay <jazzmaster0428@gmail.com>
2026-02-28 23:05:29 +0900

    [update] ダッシュボードにユーザー名とタグ一覧API叩くように修正

---
+++ src/api/user.ts
@@ -0,0 +1,11 @@
+import apiClient from './axios'
+
+export interface UserProfile {
+  userName: string
+  mailAddress: string
+}
+
+export const getUserProfile = async (): Promise<UserProfile> => {
+  const response = await apiClient.get('/api/v1/users/me')
+  return response.data
+}

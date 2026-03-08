f4eab3947b6ef7df1f40cb2981c390067e642a05
jazzpalay <jazzmaster0428@gmail.com>
2026-03-08 20:39:43 +0900

    [add] タグ管理画面の作成

--- src/api/tag.ts
+++ src/api/tag.ts
@@ -15,3 +15,28 @@ export const getTags = async (): Promise<TagListResponse> => {
   const response = await apiClient.get('/api/v1/tags/all')
   return response.data
 }
+
+export const createTag = async (tagName: string, tagColor: string): Promise<TagListResponse> => {
+  const response = await apiClient.post('/api/v1/tags/create', {
+    tagName,
+    tagColor
+  })
+  return response.data
+}
+
+export const updateTag = async (tag: Tag): Promise<void> => {
+  const response = await apiClient.put(
+    `/api/v1/tags/${tag.tagId}`,
+    {
+      tagName: tag.tagName,
+      tagColor: tag.tagColor
+    }
+  )
+
+  return response.data
+}
+
+export const deleteTag = async (tagId: string): Promise<void> => {
+  const response = await apiClient.delete(`/api/v1/tags/${tagId}`)
+  return response.data
+}
